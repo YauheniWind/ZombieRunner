b66dfe719ffa34d648684668268c033a39f90dd4
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-11-02 10:55:50 +0100

    1. Setup controllers, 2. made buttons, 3. made navigation controller

--- ZombieRunner/ViewController.swift
+++ ZombieRunner/ViewController.swift
@@ -11,9 +11,36 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
+  
+  @IBAction func startGameButtonPressed(_ sender: UIButton) {
+    
+    let storyboard = UIStoryboard(name: "GameStoryboard", bundle: nil)
+    
+    let gameViewController = storyboard.instantiateViewController(withIdentifier: "GameViewController")
+    
+    navigationController?.pushViewController(gameViewController, animated: true)
+  }
+  
+  @IBAction func recordsButtonPressed(_ sender: UIButton) {
+    
+    let storyboard = UIStoryboard(name: "TableOfRecordsStoryboard", bundle: nil)
+    
+    let recordsViewController = storyboard.instantiateViewController(withIdentifier: "RecordsViewController")
+    
+    navigationController?.pushViewController(recordsViewController, animated: true)
+    
+  }
+  
+  @IBAction func settingsButtonPressed(_ sender: UIButton) {
+    
+    let storyboard = UIStoryboard(name: "SettingsStoryboard", bundle: nil)
+    
+    let settingsViewController = storyboard.instantiateViewController(withIdentifier: "SettingsViewController")
+    
+    navigationController?.pushViewController(settingsViewController, animated: true)
+  }
+  
 }
 
